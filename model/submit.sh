94833986d57690439f136d57c40473c7a424e326
Asa_Barth-Maron <asabarthmaron@gmail.com>
2019-03-11 15:45:15 -0400

    Slurm outfiles are now put in a separate directory.

--- model/submit.sh
+++ model/submit.sh
@@ -5,6 +5,7 @@
 #SBATCH -c 2
 #SBATCH --mail-user=asabarthmaron@gmail.com
 #SBATCH --mail-type=NONE
+#SBATCH -o "/home/anb12/Modeling/modeling_results/slurm_outfiles/%j.out"
 
 module load matlab/2018a
 matlab -nodisplay -r "cd /home/anb12/Modeling/modeling_code/model; run_model_O2('$1', $2, '$3')"
